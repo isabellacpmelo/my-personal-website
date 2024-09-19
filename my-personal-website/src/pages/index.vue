565d44d4f7627f81c3a3c663cc4cb8ed7a94f48e
isabellacpmelo <isacpmelo@gmail.com>
2024-09-19 20:08:04 -0300

    feat: Setting routes file and css file

---
+++ my-personal-website/src/pages/index.vue
@@ -0,0 +1,14 @@
+// eslint-disable-next-line vue multi-word-component-names
+<script>
+console.log('Oi')
+</script>
+<template>
+    <h1>Hello World...</h1>
+</template>
+
+<style>
+h1 {
+    color: white;
+    font-size: 4rem;
+}
+</style>
