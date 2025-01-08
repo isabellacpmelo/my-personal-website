5497a117bc14813521a2c806fca7399c341e33d2
isabellacpmelo <isacpmelo@gmail.com>
2025-01-08 19:37:17 -0300

    feat: Adding profile pic

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,10 @@
 console.log("Oi");
 </script>
 <template>
-  <h1>Hello World...</h1>
+  <div class="flex justify-center items-center gap-8">
+    <img src="/src/assets/img/my-pic.png" alt="My profile pic" class="h-52" />
+    <h1>Olá, eu sou Isabella Melo :)</h1>
+  </div>
 </template>
 
 <!-- <style>
