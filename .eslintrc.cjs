b704221195a0b8e79e24f0e337f6d5d41ae1ef48
isabellacpmelo <isacpmelo@gmail.com>
2025-02-18 20:11:52 -0300

    feat: Adding new structures

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,17 @@
-/* eslint-env node */
+/**
+ * eslint-env node
+ *
+ * @format
+ */
+
+// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
+};
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,5 @@
 <!-- @format -->
 
-// eslint-disable-next-line vue multi-word-component-names
 <script setup>
 import { ref, onMounted } from "vue";
 
@@ -43,7 +42,7 @@ onMounted(() => {
 
 <template>
   <div
-    class="w-full h-[400px] flex justify-center items-center bg-black/10 shadow-md shadow-black/30">
+    class="w-full h-[300px] flex justify-center items-center bg-black/10 shadow-md shadow-black/30">
     <div class="text-3xl">
       <p class="text-white"><span v-text="displayText1"></span></p>
       <p class="text-white">
@@ -57,4 +56,61 @@ onMounted(() => {
       alt="My profile pic"
       class="h-72" />
   </div>
+  <div
+    class="w-full h-full p-16 flex flex-col justify-start items-start bg-black/10 shadow-md shadow-black/30 gap-8">
+    <h2 class="text-3xl text-white">Sobre mim</h2>
+    <!-- fazer uma breve apresentação -->
+    <!-- adicionar imagens -->
+    <div
+      class="flex flex-col justify-center items-center w-full text-white gap-8">
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pretium
+        arcu porttitor volutpat accumsan. Quisque mi nunc, ullamcorper quis
+        porta eu, condimentum eu quam. Etiam sit amet elementum sapien. Integer
+        condimentum mi eget lobortis pharetra. Donec purus est, ullamcorper non
+        est id, posuere condimentum metus. Nam interdum orci et justo ultrices,
+        eu euismod urna gravida. Fusce sagittis dolor a urna sollicitudin
+        suscipit.
+      </div>
+      <div>
+        Ut gravida est nec enim ultricies, eget vehicula nunc hendrerit.
+        Suspendisse potenti. Donec lobortis, erat in venenatis posuere, odio
+        odio dapibus libero, vitae condimentum ligula nisl id dui. Curabitur
+        condimentum placerat quam in elementum. Praesent ut convallis mauris.
+        Duis eget augue ac ante pulvinar molestie. In sed augue ipsum.
+        Pellentesque sit amet massa porta, mattis quam non, semper diam. In hac
+        habitasse platea dictumst. Etiam auctor ac est et pellentesque. Proin
+        tincidunt luctus nisi in tempor. Ut ullamcorper, sapien vitae tempor
+        eleifend, nulla mi posuere neque, eu hendrerit purus augue eget massa.
+        Nam sodales augue urna, eget auctor felis efficitur ac. Fusce elementum
+        imperdiet ante non porta. Suspendisse et dui non erat egestas blandit.
+      </div>
+    </div>
+  </div>
+  <div
+    class="w-full p-16 flex flex-col justify-start items-start bg-black/10 shadow-md shadow-black/30 gap-8">
+    <h2 class="text-3xl text-white">Meus projetos</h2>
+    <div class="flex justify-center items-center w-full text-white gap-8">
+      <div>Projeto 1</div>
+      <div>Projeto 2</div>
+      <div>Projeto 3</div>
+      <div>Projeto 4</div>
+      <div>Projeto 5</div>
+      <div>Projeto 6</div>
+      <div>Projeto 7</div>
+      <div>Projeto 8</div>
+    </div>
+    <!-- criar um carrossel com imagens dos projetos, fazer breve apresentação, adicionar tags, adicionar link do github -->
+  </div>
+  <div
+    class="w-full p-16 flex flex-col justify-start items-start bg-black/10 shadow-md shadow-black/30 gap-8">
+    <h2 class="text-3xl text-white">Entre em contato</h2>
+    <div class="flex justify-center items-center w-full text-white gap-8">
+      <div>Linkedin</div>
+      <div>Github</div>
+      <div>Email</div>
+    </div>
+    <!-- adicionar link do github, adicionar linkedin e email -->
+  </div>
+  <!-- adiconar footer aqui -->
 </template>
