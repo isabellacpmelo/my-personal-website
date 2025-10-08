39455a0222dcb7d7a9be7f7b1e33d70e932d159a
isabellacpmelo <isacpmelo@gmail.com>
2025-10-07 22:25:07 -0300

    feat: Remodeling component structure to section

--- src/components/UI/Section.vue
+++ src/components/UI/Section.vue
@@ -5,21 +5,29 @@ const props = defineProps({
   title: {
     type: String,
   },
-  sectionClass: {
-    type: String,
+  light: {
+    type: Boolean,
+    default: false,
   },
-  padding: {
-    type: String,
-    default: "px-8 lg:px-16 2xl:px-48 py-16 lg:py-24",
-  },
-});
+})
 </script>
 
 <template>
   <div
-    class="w-full h-full flex flex-col justify-start items-center lg:items-start shadow-sm shadow-black/20 gap-8"
-    :class="[sectionClass, padding]">
-    <h2 v-if="title" class="text-3xl lg:text-5xl text-white">{{ title }}</h2>
+    class="h-screen w-full flex flex-col justify-start items-center md:items-start shadow-sm shadow-black/20 gap-8 p-4 sm:p-8 md:p-12 lg:p-16 xl:p-24 text-sm md:text-base"
+    :class="{
+      'bg-secondary text-darkText': light,
+      'bg-primary text-lightText': !light,
+    }">
+    <h2
+      v-if="title"
+      class="text-3xl lg:text-5xl"
+      :class="{
+        'text-primary': light,
+        'text-secondary': !light,
+      }">
+      {{ title }}
+    </h2>
     <slot />
   </div>
 </template>
