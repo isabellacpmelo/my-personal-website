dfca8da887548ad6129747f021022519c11f4774
isabellacpmelo <isacpmelo@gmail.com>
2025-10-07 22:24:31 -0300

    feat: Creating a component for a profile image

---
+++ src/components/UI/ProfilePic.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  isMinimized: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <div
+    class="bg-mustard rounded-full"
+    :class="{
+      'h-10 w-10 md:w-12 md:h-12': isMinimized,
+      'h-44 w-44 md:h-52 md:w-52 lg:h-64 lg:w-64 xl:h-96 xl:w-96': !isMinimized,
+    }">
+    <img src="@/assets/img/profile-pic.png" />
+  </div>
+</template>
