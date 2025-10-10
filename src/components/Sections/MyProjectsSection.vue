34d05eac658179e405de0fbb9bf8d388defb9ab1
isabellacpmelo <isacpmelo@gmail.com>
2025-10-10 20:06:49 -0300

    feat: starting to create components to wrap project images

--- src/components/Sections/MyProjectsSection.vue
+++ src/components/Sections/MyProjectsSection.vue
@@ -4,5 +4,28 @@
     illo sapiente, minima harum reprehenderit explicabo eaque neque at, fugiat
     quam! Eligendi ipsam voluptate, asperiores tempore dicta sequi dolore
     dolorem?
+
+    <div class="border-2 border-primary h-40 w-80">
+      <div
+        class="bg-primary w-full h-[10%] flex justify-between items-center px-1.5">
+        <div class="h-full flex items-center gap-1">
+          <div class="w-2 h-2 bg-lightText rounded-full" />
+          <div class="w-2 h-2 bg-lightText rounded-full" />
+          <div class="w-2 h-2 bg-lightText rounded-full" />
+        </div>
+        <div class="w-4 h-2 bg-lightText rounded-full" />
+      </div>
+      <div class="flex justify-between h-[90%]">
+        <div>
+          <!-- <img
+            src="/public/img/projects-cover/cripto-coin.png"
+            alt=""
+            class="h-full w-80 object-cover" /> -->
+        </div>
+        <div class="border-l border-primary w-4 h-full flex items-start">
+          <div class="bg-primary h-12 w-4 mt-0.5" />
+        </div>
+      </div>
+    </div>
   </Section>
 </template>
