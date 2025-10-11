99bb81859d7b2e2e02a94882d0a802a8fa950dec
isabellacpmelo <isacpmelo@gmail.com>
2025-10-11 19:59:08 -0300

    feat: Rearranging project images

--- src/components/Sections/MyProjectsSection.vue
+++ src/components/Sections/MyProjectsSection.vue
@@ -1,3 +1,7 @@
+<script setup>
+import criptoCoinCover from '@/assets/img/projects-cover/cripto-coin.png'
+</script>
+
 <template>
   <Section title="Meus Projetos" light>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur facere
@@ -5,27 +9,15 @@
     quam! Eligendi ipsam voluptate, asperiores tempore dicta sequi dolore
     dolorem?
 
-    <div class="border-2 border-primary h-40 w-80">
-      <div
-        class="bg-primary w-full h-[10%] flex justify-between items-center px-1.5">
-        <div class="h-full flex items-center gap-1">
-          <div class="w-2 h-2 bg-lightText rounded-full" />
-          <div class="w-2 h-2 bg-lightText rounded-full" />
-          <div class="w-2 h-2 bg-lightText rounded-full" />
-        </div>
-        <div class="w-4 h-2 bg-lightText rounded-full" />
-      </div>
-      <div class="flex justify-between h-[90%]">
-        <div>
-          <!-- <img
-            src="/public/img/projects-cover/cripto-coin.png"
-            alt=""
-            class="h-full w-80 object-cover" /> -->
-        </div>
-        <div class="border-l border-primary w-4 h-full flex items-start">
-          <div class="bg-primary h-12 w-4 mt-0.5" />
-        </div>
-      </div>
+    <div class="flex gap-9">
+      <ImageFrameComputer
+        :src="criptoCoinCover"
+        alt="teste"
+        class="hidden sm:inline" />
+      <ImageFrameMobile
+        :src="criptoCoinCover"
+        alt="teste"
+        class="inline sm:hidden" />
     </div>
   </Section>
 </template>
