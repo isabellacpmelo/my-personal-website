dcad11dcd6b6171390f549b97f3bf4a626410bc1
isabellacpmelo <isacpmelo@gmail.com>
2025-10-13 22:06:33 -0300

    feat: Improving image frame components

--- src/components/UI/ImageFrameComputer.vue
+++ src/components/UI/ImageFrameComputer.vue
@@ -10,35 +10,38 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="border-2 border-lightBlue h-40 w-80 group">
-    <div
-      class="bg-lightBlue group-hover:bg-primary w-full h-[10%] flex justify-between items-center px-1 transition duration-700 ease-in-out border-[0.5px] border-lightBlue">
-      <div class="h-full flex items-center gap-1">
-        <div
-          class="w-2 h-2 bg-lightText group-hover:bg-red-300 rounded-full group-hover:animate-pulse transition duration-700 ease-in-out delay-75" />
-        <div
-          class="w-2 h-2 bg-lightText group-hover:bg-yellow-300 rounded-full group-hover:animate-pulse transition duration-700 ease-in-out delay-100" />
-        <div
-          class="w-2 h-2 bg-lightText group-hover:bg-green-300 rounded-full group-hover:animate-pulse transition duration-700 ease-in-out delay-150" />
-      </div>
+  <div>
+    <div class="border-2 border-lightBlue h-36 w-72 group">
       <div
-        class="h-full flex items-center group-hover:opacity-75 transition duration-700 ease-in-out">
-        <div class="text-lightText bi-dash" />
-        <div class="text-lightText bi-x" />
-      </div>
-    </div>
-    <div class="flex justify-between h-[90%] w-full overflow-hidden">
-      <div class="w-[97%] overflow-hidden">
-        <img
-          :src="src"
-          :alt="alt"
-          v-bind="$attrs"
-          class="w-full h-full object-cover object-center group-hover:scale-[1.01] transition duration-300 ease-in-out" />
-      </div>
-
-      <div class="border-l border-lightBlue w-[3%] h-full flex items-start">
+        class="bg-lightBlue group-hover:bg-primary w-full h-[10%] flex justify-between items-center px-1 transition duration-700 ease-in-out border-[0.5px] border-lightBlue">
+        <div class="h-full flex items-center gap-1">
+          <div
+            class="w-2 h-2 bg-lightText group-hover:bg-red-300 rounded-full group-hover:animate-pulse transition duration-700 ease-in-out delay-75" />
+          <div
+            class="w-2 h-2 bg-lightText group-hover:bg-yellow-300 rounded-full group-hover:animate-pulse transition duration-700 ease-in-out delay-100" />
+          <div
+            class="w-2 h-2 bg-lightText group-hover:bg-green-300 rounded-full group-hover:animate-pulse transition duration-700 ease-in-out delay-150" />
+        </div>
         <div
-          class="bg-lightBlue h-12 w-full mt-2 group-hover:bg-primary transition duration-300 ease-in-out" />
+          class="h-full flex items-center group-hover:opacity-75 transition duration-700 ease-in-out">
+          <div class="text-lightText bi-dash" />
+          <div class="text-lightText bi-x" />
+        </div>
+      </div>
+      <div class="flex justify-between h-[90%] w-full overflow-hidden">
+        <div class="w-[97%] overflow-hidden">
+          <img
+            :src="src"
+            :alt="alt"
+            v-bind="$attrs"
+            class="w-full h-full object-cover object-center group-hover:scale-[1.01] transition duration-300 ease-in-out" />
+        </div>
+
+        <div
+          class="border-l border-lightBlue w-[3%] h-full flex items-start bg-white">
+          <div
+            class="bg-lightBlue h-12 w-full mt-2 group-hover:bg-primary transition duration-300 ease-in-out" />
+        </div>
       </div>
     </div>
   </div>
