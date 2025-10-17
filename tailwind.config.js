c1ecd9c77b9c99eb5ebb9456926daec24dae3e3d
isamelo <42364778+isabellacpmelo@users.noreply.github.com>
2025-10-17 14:25:53 -0300

    fix: Clean up formatting in tailwind.config.js for improved readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,30 +4,29 @@
  */
 
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#200934',
-        secondary: '#f9f7f0',
-        mustard: '#f8b454',
-        lightText: '#e3e2d6',
-        darkText: '#09090b',
-        lightBlue: '#5e5ba9',
+        primary: "#200934",
+        secondary: "#f9f7f0",
+        mustard: "#f8b454",
+        lightText: "#e3e2d6",
+        darkText: "#09090b",
+        lightBlue: "#5e5ba9",
       },
       keyframes: {
         shake: {
-          '0%, 100%': { transform: 'translateX(0)' },
-          '25%': { transform: 'translateX(-0.5px)' },
-          '50%': { transform: 'translateX(0.5px)' },
-          '75%': { transform: 'translateX(-0.5px)' },
+          "0%, 100%": { transform: "translateX(0)" },
+          "25%": { transform: "translateX(-0.5px)" },
+          "50%": { transform: "translateX(0.5px)" },
+          "75%": { transform: "translateX(-0.5px)" },
         },
       },
       animation: {
-        shake: 'shake 0.6s ease-in-out',
+        shake: "shake 0.6s ease-in-out",
       },
     },
   },
   plugins: [],
-}
+};
